0db299cad6e7bca29d7170b489924f9896dfe854
Owen Tsao <richard.tsao@live.cn>
2016-02-05 21:32:06 +1300

    add scenario for AutoComplete

---
+++ src/main/java/com/helloowen/angularmaterialtest/UserBehavior.java
@@ -0,0 +1,12 @@
+package com.helloowen.angularmaterialtest;
+
+/**
+ * Created by caoyunxiang on 2/5/16.
+ */
+public class UserBehavior {
+    @cucumber.api.java.en.Given("^I visit a page with AutoComplete$")
+    public void I_visit_a_page_with_AutoComplete() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new cucumber.api.PendingException();
+    }
+}
